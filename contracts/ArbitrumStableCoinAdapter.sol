1714fdcd4d72260dbf30044b7fdcd82fffefb287
Satya Bhaskar <satyabhaskar@Satyas-MacBook-Air.local>
2024-07-20 15:54:29 +0530

    added tests

--- contracts/ArbitrumStableCoinAdapter.sol
+++ contracts/ArbitrumStableCoinAdapter.sol
@@ -2,13 +2,10 @@
 pragma solidity ^0.8.22;
 
 
-// import "https://github.com/LayerZero-Labs/LayerZero-v2/blob/main/packages/layerzero-v2/evm/oapp/contracts/oft/OFTAdapter.sol"
-
-
 import { OFTAdapter } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFTAdapter.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ArbitrumStableCoinAdapter is OFTAdapter {
+contract ArbitrumAdapter is OFTAdapter {
     constructor(
         address _token, // a deployed, already existing ERC20 token address
         address _layerZeroEndpoint, // local endpoint address
--- scripts/DeployArbitrum.sol
+++ scripts/DeployArbitrum.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.22;
 
 import {Script} from "forge-std/Script.sol";
-import {ArbitrumStableCoinAdapter} from "../contracts/ArbitrumStableCoinAdapter.sol";
+import {ArbitrumAdapter} from "../contracts/ArbitrumStableCoinAdapter.sol";
 
 contract DeployOnArbitrum is Script{
 
@@ -10,8 +10,8 @@ contract DeployOnArbitrum is Script{
 
     function run() external{
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-        ArbitrumStableCoinAdapter arbitrumStableCoinAdapter = new ArbitrumStableCoinAdapter(
-        0x966570A84709D693463CDD69dCadb0121b2C9d26,/*mintable token address */
+        ArbitrumAdapter arbitrumAdapter = new ArbitrumAdapter(
+        0x292b34fB4737908a73CedF028df1cB4f7b38f3B9,/*mintable token address */
         0x6EDCE65403992e310A62460808c4b910D972f10f,/*layerzero end point address */
         msg.sender);
         vm.stopBroadcast();
